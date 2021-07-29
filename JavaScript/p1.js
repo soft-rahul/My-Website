0d1d9f2d224e962d0db8de190c1fdafbf5b50309
RAHUL KESHRI <rkeshri2021@gmail.com>
2021-07-29 23:39:58 +0530

    Initial Project

---
+++ JavaScript/p1.js
@@ -0,0 +1,15 @@
+function openMenu() {
+
+    var ele;
+    ele = document.getElementById('js-dropdown-menu');
+    ele.style.visibility = 'visible';
+
+
+}
+function closeMenu() {
+
+    var eleTwo;
+    eleTwo = document.getElementById('js-dropdown-menu');
+    eleTwo.style.display = 'none';
+
+}
