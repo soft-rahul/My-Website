c87e91c6b80fc9ec1c2d4d3025480b3a2399f841
RAHUL KESHRI <rkeshri2021@gmail.com>
2021-08-10 16:12:45 +0530

    All done

--- JavaScript/p1.js
+++ JavaScript/p1.js
@@ -1,15 +1,20 @@
 function openMenu() {
 
-    var ele;
-    ele = document.getElementById('js-dropdown-menu');
+    let ele;
+    ele = document.querySelector('#js-dropdown-menu');
     ele.style.visibility = 'visible';
 
 
 }
 function closeMenu() {
 
-    var eleTwo;
-    eleTwo = document.getElementById('js-dropdown-menu');
+    let eleTwo;
+    eleTwo = document.querySelector('#js-dropdown-menu');
     eleTwo.style.display = 'none';
 
 }
+const closeBtn = document.querySelector('#js-menu-close-btn');
+closeBtn.addEventListener('click',closeMenu);
+
+const openBtn = document.querySelector('#open');
+openBtn.addEventListener('click',openMenu), {times:Infinity};
